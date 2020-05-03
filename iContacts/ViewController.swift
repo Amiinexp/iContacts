7aa3ab431c98579b0c0dbaaf39ed5e813f2f7efe
Yousef <yosefali97@hotmail.com>
2020-05-03 14:49:12 +0200

    Initial Commit

---
+++ iContacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iContacts
+//
+//  Created by Yousef on 5/3/20.
+//  Copyright © 2020 Yousef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
