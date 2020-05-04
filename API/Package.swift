14d2e41b68eaa4bc3c4c521e787fa7fd5916e5b3
Yousef <yosefali97@hotmail.com>
2020-05-04 19:41:10 +0200

    API Ready

---
+++ API/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "iContactsAPI",
+    products: [
+        .library(name: "iContactsAPI", targets: ["App"]),
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+
+        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0"),
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "JWT"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"])
+    ]
+)
+
--- iOS App/iContacts/ViewController.swift
+++ iOS App/iContacts/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ContactsUI
 
 class ViewController: UIViewController {
 
